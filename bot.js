da90ce6f64253d5ea91b9c291780796af64d1ab0
OutlookG <37785930+OutlookG@users.noreply.github.com>
2018-10-03 07:50:39 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,3 +3,11 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 bot.login('NDc4MDA2NDQ2Mjc4MTgwODc2.DpZYDw.QhZ-9f8xj54yAclVxwe-OMIUJ7w');
+
+Bot.on('message', (message) => {
+   if(message.author.id == 427623796456030208) return;
+  
+   if(message.content == "!verifyhelp") {
+     message.reply('be sure to check out #welcome to learn how to verify!')
+   };
+});
