4284c3b506efa48a4fc43ecb06021e9bd4bd5613
OutlookG <37785930+OutlookG@users.noreply.github.com>
2018-10-03 18:24:44 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const bot = new Discord.Client();
 bot.login('NDc4MDA2NDQ2Mjc4MTgwODc2.DpZYDw.QhZ-9f8xj54yAclVxwe-OMIUJ7w');
 
 bot.on('message', (message) => {
-   if(message.author.id == 427623796456030208) return;
+   if(message.author.bot) return;
   
    if(message.content == "!verifyhelp") {
      message.reply('be sure to check out #welcome to learn how to verify!')
@@ -14,7 +14,7 @@ bot.on('message', (message) => {
    if(message.content == "!requestsoff") {
       if (message.member.roles.find('name','DiscordRequestsOn')) {
          var role = message.guild.roles.find('name','DiscordRequestsOn')
-         user.removeRole(role)
+         message.member.removeRole(role)
       } else {
          message.reply('your requests are allready off!')
       };
@@ -24,7 +24,7 @@ bot.on('message', (message) => {
          message.reply('you already have them on!')
       } else {
          var role = message.guild.roles.find('name','DiscordRequestsOn')
-         message.author.addRole(role)
+         message.member.addRole(role)
       };
    };
 });
